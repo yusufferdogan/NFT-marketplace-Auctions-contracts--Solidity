a68446106b037a4f5e9ec3b8addda85f614a15ab
Ahmet Yusuf Erdoğan <yusuferdogan@bixos.com>
2022-07-05 23:46:52 +0300

    create dutch auction and tests closes #1

---
+++ contracts/DutchAuction.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+
+// console.log() @TODO: remove that
+import "hardhat/console.sol";
+
+/**
+ * @title English Auction contract for NFT's
+ * @author @yusufferdogan
+ * @notice All NFT contracts are accepted
+ * @dev You must use _safeMint or _safeTransferFrom to use this contract otherwise you nft will be unreachable
+ */
+contract DutchAuction {
+    struct Auction {
+        address seller;
+        uint256 startPrice;
+        uint256 startDate;
+        uint256 discountRate;
+        uint256 duration;
+    }
+
+    error NotOwner();
+    error InsufficientFunds();
+    error AuctionEnded();
+    error TransferTxError();
+    error InvalidStartingPrice();
+
+    event AuctionCreated(
+        address indexed seller,
+        address indexed contractAddress,
+        uint256 indexed tokenId
+    );
+
+    event Sold(
+        address indexed contractAddress,
+        uint256 indexed tokenId,
+        address indexed bidder,
+        uint256 price
+    );
+
+    // contract Address => tokenId => Auction
+    mapping(address => mapping(uint256 => Auction)) public auctions;
+
+    uint256 public platformFees;
+
+    uint256 public constant FEE_PERCANTAGE = 5;
+
+    function createAuction(
+        address contractAddress,
+        uint256 tokenId,
+        uint256 startPrice,
+        uint256 discountRate,
+        uint256 duration
+    ) external returns (uint256) {
+        if (startPrice < discountRate * duration) revert InvalidStartingPrice();
+
+        auctions[contractAddress][tokenId] = Auction(
+            msg.sender,
+            startPrice,
+            // solhint-disable-next-line
+            block.timestamp,
+            discountRate,
+            duration
+        );
+        emit AuctionCreated(msg.sender, contractAddress, tokenId);
+        return tokenId;
+    }
+
+    function getPrice(address contractAddress, uint256 tokenId)
+        public
+        view
+        returns (uint256)
+    {
+        Auction memory auction = auctions[contractAddress][tokenId];
+        // solhint-disable-next-line not-rely-on-time
+        uint256 timeElapsed = block.timestamp - auction.startDate;
+        uint256 discount = auction.discountRate * timeElapsed;
+        return auction.startPrice - discount;
+    }
+
+    // your total bid must be greater than biggest bid
+    function buy(address contractAddress, uint256 tokenId) external payable {
+        Auction memory auction = auctions[contractAddress][tokenId];
+        //solhint-disable-next-line not-rely-on-time
+        if (block.timestamp > auction.startDate + auction.duration)
+            revert AuctionEnded();
+
+        uint256 price = getPrice(contractAddress, tokenId);
+
+        if (msg.value < price) revert InsufficientFunds();
+
+        IERC721(contractAddress).transferFrom(
+            auction.seller,
+            msg.sender,
+            tokenId
+        );
+
+        platformFees += (msg.value * FEE_PERCANTAGE) / 100;
+
+        //solhint-disable-next-line avoid-low-level-calls
+        (bool isSuccess, ) = payable(msg.sender).call{
+            value: (msg.value * (100 - FEE_PERCANTAGE)) / 100
+        }("");
+
+        if (!isSuccess) revert TransferTxError();
+
+        emit Sold(contractAddress, tokenId, msg.sender, msg.value);
+    }
+}
---
+++ test/DutchAuction.test.ts
@@ -0,0 +1,105 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { Contract, ContractFactory, BigNumber } from 'ethers';
+import { ethers } from 'hardhat';
+
+interface CreateAuctionScenario {
+  _nftContract?: Contract;
+  _minter?: SignerWithAddress;
+  _startPrice?: BigNumber;
+  _discountRate?: BigNumber;
+  _duration?: BigNumber;
+}
+
+describe('DutchAuction', () => {
+  let contract: Contract;
+  let nftContract: Contract;
+  let owner: SignerWithAddress;
+  let addresses: SignerWithAddress[];
+  let factory: ContractFactory;
+  let nftFactory: ContractFactory;
+  const MINT_PRICE = ethers.utils.parseEther('0.001');
+
+  // hooks
+  before(async () => {
+    [owner, ...addresses] = await ethers.getSigners();
+    nftFactory = await ethers.getContractFactory('Foo721');
+    factory = await ethers.getContractFactory('DutchAuction');
+  });
+
+  beforeEach(async () => {
+    const now = Math.floor(+new Date() / 1000);
+    nftContract = await nftFactory.deploy(now - 100);
+    contract = await factory.deploy();
+  });
+
+  async function createAuctionScenario({
+    _nftContract = nftContract,
+    _minter = owner,
+    _startPrice = BigNumber.from('1000'),
+    _duration = BigNumber.from('1000'),
+    _discountRate = BigNumber.from('1'),
+  }: CreateAuctionScenario = {}) {
+    await _nftContract.mint(_minter.address, 1, { value: MINT_PRICE });
+    await _nftContract.approve(contract.address, 1);
+
+    return await contract
+      .connect(_minter)
+      .createAuction(
+        _nftContract.address,
+        1,
+        _startPrice,
+        _discountRate,
+        _duration
+      );
+  }
+
+  it('should buy nft (Sold Event)', async () => {
+    await createAuctionScenario();
+    const price = await contract.getPrice(nftContract.address, 1);
+
+    await expect(
+      contract.connect(addresses[5]).buy(nftContract.address, 1, {
+        value: price,
+      })
+    )
+      .to.emit(contract, 'Sold')
+      .withArgs(nftContract.address, 1, addresses[5].address, price);
+  });
+
+  it('should buy nft (Transfer Event)', async () => {
+    await createAuctionScenario();
+    const price = await contract.getPrice(nftContract.address, 1);
+
+    await expect(
+      contract.connect(addresses[5]).buy(nftContract.address, 1, {
+        value: price,
+      })
+    )
+      .to.emit(nftContract, 'Transfer')
+      .withArgs(owner.address, addresses[5].address, 1);
+  });
+
+  it('should not buy nft if auction is ended', async () => {
+    await createAuctionScenario();
+    const price = await contract.getPrice(nftContract.address, 1);
+
+    await ethers.provider.send('evm_increaseTime', [10000]);
+
+    await expect(
+      contract.connect(addresses[5]).buy(nftContract.address, 1, {
+        value: price,
+      })
+    ).to.be.revertedWith('AuctionEnded');
+
+    await ethers.provider.send('evm_increaseTime', [-10000]);
+  });
+
+  it('should not buy nft if Insufficient Funds', async () => {
+    await createAuctionScenario();
+
+    await expect(
+      contract.connect(addresses[5]).buy(nftContract.address, 1)
+    ).to.be.revertedWith('InsufficientFunds');
+  });
+});
